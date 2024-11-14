95b152c349261bd5e17741216d32a42f67502fde
Zhao Hsin Rui (Ray) <bill56523@gmail.com>
2024-11-14 14:39:40 +0800

    LLM 節點按鈕

---
+++ src/app/nodes/llm-node/llm-node.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-llm-node',
+  standalone: true,
+  imports: [],
+  templateUrl: './llm-node.component.html',
+  styleUrl: './llm-node.component.css'
+})
+export class LlmNodeComponent {
+  
+}
