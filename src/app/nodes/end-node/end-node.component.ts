47ac1a909aeb62ed663b846511df2c5f054d94a2
Zhao Hsin Rui (Ray) <bill56523@gmail.com>
2024-11-14 14:41:16 +0800

    結束按鈕

---
+++ src/app/nodes/end-node/end-node.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-node',
+  standalone: true,
+  imports: [],
+  templateUrl: './end-node.component.html',
+  styleUrl: './end-node.component.css'
+})
+export class EndNodeComponent {
+
+}
